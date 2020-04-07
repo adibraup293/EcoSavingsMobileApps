168540051e8f9fd51ff2255aaf83e430ce051a55
adibraup293 <36180086+adibraup293@users.noreply.github.com>
2020-04-07 12:33:18 +0800

    Designing home page

---
+++ EcoSavingsMobileApps/EcoSavingsMobileApps/ViewModel/HomeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using EcoSavingsMobileApps.Models;
+using EcoSavingsMobileApps.Utilities;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace EcoSavingsMobileApps.ViewModel
+{
+    class HomeViewModel : INotifyPropertyChanged
+    {
+        private void OpenSignUpExecute(object obj)
+        {
+            
+            Application.Current.MainPage.Navigation.PushAsync(
+                new Views.SignUpView());
+        }
+    }
+}
